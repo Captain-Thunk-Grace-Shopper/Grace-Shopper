9c4a3985f53f7e6d2cc05ef57b5fa8f80f3c7380
Wesam Abdelzaher <wesam.abdelzaher0@gmail.com>
2019-11-05 17:44:59 -0500

    product routes and component test

---
+++ client/components/Products.spec.js
@@ -0,0 +1,22 @@
+/* global describe beforeEach it */
+
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Products from './Products'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('Products', () => {
+  let products
+
+  beforeEach(() => {
+    products = shallow(<Products products={{name: 'bananas'}} />)
+  })
+
+  it('renders the product name in an h5', () => {
+    expect(products.find('h5').text()).to.be.equal('bananas')
+  })
+})
---
+++ server/api/product.spec.js
@@ -0,0 +1,38 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const Product = db.model('product')
+
+describe('Product routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('/api/products/', () => {
+    const productName = 'bananas'
+    const productPrice = 10.0
+    const productCategory = 'Fruit'
+
+    beforeEach(() => {
+      return Product.create({
+        name: productName,
+        price: productPrice,
+        category: productCategory
+      })
+    })
+
+    it('GET /api/products', async () => {
+      const res = await request(app)
+        .get('/api/products')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].name).to.be.equal(productName)
+      expect(res.body[0].price).to.be.equal(productPrice.toString())
+      expect(res.body[0].category).to.be.equal(productCategory)
+    })
+  }) // end describe('/api/users')
+}) // end describe('User routes')
