1102d4469ba2218095fc6292499881374b9ceaa5
kpryzk <kepryzk@gmail.com>
2019-11-07 12:07:50 -0500

    created two routes for finding all order items

--- server/api/order-items.js
+++ server/api/order-items.js
@@ -3,21 +3,22 @@ const {OrderItem} = require('../db/models')
 const {Product} = require('../db/models')
 module.exports = router
 
-//find all ORDER ITEMS in an order
-// router.get('/:orderId', async (req, res, next) => {
-//   try {
-//     const items = await OrderItem.findAll()
-//     res.json(items)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
+//find ALL ITEMS EVER ORDERED - probably not necessary
+router.get('/', async (req, res, next) => {
+  try {
+    const items = await OrderItem.findAll()
+    res.json(items)
+  } catch (err) {
+    next(err)
+  }
+})
 
+//find ALL ORDER ITEMS in one order
 router.get('/:orderId', async (req, res, next) => {
   try {
     const items = await OrderItem.findAll({
       where: {
-        id: req.params.orderId
+        orderId: req.params.orderId
       }
     })
     res.json(items)
