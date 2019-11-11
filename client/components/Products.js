d3ac6f7af468f419b6c09c0ebb82ce589df838e7
kpryzk <kepryzk@gmail.com>
2019-11-11 12:34:58 -0500

    added ability to change quantity with plus minus buttons on product page, display and edit quantity input on product page, change price in cart to total which takes into account quantity, and added total sum of values to cart

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,33 +1,73 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
-const Products = props => {
-  const productName = props.products.name
-  const productId = props.products.id
-  const quantity = 1
-  const price = props.products.price
-  return (
-    <div>
-      <Link to={`/products/${props.products.id}`}>
-        <div id="grocery-list-image-container">
-          <img src={props.products.imgUrl} id="grocery-list-image" />
-        </div>
-        <div id="grocery-list-name">
-          <p>
-            <b>{props.products.name}</b>
-          </p>
-        </div>
-      </Link>
-      <button
-        type="button"
-        onClick={() => {
-          props.addToCart(productName, quantity, price)
-        }}
-      >
-        Add to cart
-      </button>
-    </div>
-  )
-}
+class Products extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quantity: 1
+    }
+    this.minus = this.minus.bind(this)
+    this.plus = this.plus.bind(this)
+  }
 
+  plus() {
+    this.setState(previousState => ({quantity: previousState.quantity + 1}))
+  }
+
+  minus() {
+    if (this.state.quantity > 0) {
+      this.setState(previousState => ({quantity: previousState.quantity - 1}))
+    }
+  }
+
+  render() {
+    const productName = this.props.products.name
+    const productId = this.props.products.id
+    const quantity = this.state.quantity
+    const price = this.props.products.price
+    return (
+      <div>
+        <Link to={`/products/${productId}`}>
+          <div id="grocery-list-image-container">
+            <img src={this.props.products.imgUrl} id="grocery-list-image" />
+          </div>
+          <div id="grocery-list-name">
+            <p>
+              <b>{productName}</b>
+            </p>
+          </div>
+        </Link>
+        <div id="quantity-container">
+          <input
+            type="button"
+            value="-"
+            className="button-minus"
+            onClick={this.minus}
+          />
+          <input
+            type="number"
+            name="quantity"
+            value={this.state.quantity}
+            onChange={evt => this.setState({quantity: evt.target.value})}
+          />
+          <input
+            type="button"
+            value="+"
+            className="button-plus"
+            onClick={this.plus}
+          />
+        </div>
+        <button
+          type="button"
+          onClick={() => {
+            this.props.addToCart(productName, quantity, price)
+          }}
+        >
+          Add to cart
+        </button>
+      </div>
+    )
+  }
+}
 export default Products
