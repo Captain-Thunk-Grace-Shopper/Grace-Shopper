7e4caed7eae05b8836059afa80456455680cf62e
kpryzk <kepryzk@gmail.com>
2019-11-12 17:58:56 -0500

    adds a link to Link to prevent everything from breaking

--- client/routes.js
+++ client/routes.js
@@ -8,9 +8,7 @@ import {
   UserHome,
   Home,
   SingleProduct,
-  Checkout,
-  Payment,
-  LastPage
+  Checkout
 } from './components'
 import Cart from './components/cart'
 import {me} from './store'
@@ -31,13 +29,15 @@ class Routes extends Component {
         <Switch>
           <div id="main-app-body">
             {/* Routes placed here are available to all visitors */}
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/payment" component={Payment} />
-        <Route path="/lastpage" component={LastPage} />
-        <Route path="/products/:productId" component={SingleProduct} />
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route
+              exact
+              path="/products/:productId"
+              component={SingleProduct}
+            />
             {isLoggedIn && (
               <Switch>
                 {/* Routes placed here are only available after logging in */}
