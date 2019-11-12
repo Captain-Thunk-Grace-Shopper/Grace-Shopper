9c20647af0fe06ac576e4736f81848def80bdfde
kpryzk <kepryzk@gmail.com>
2019-11-12 16:58:54 -0500

    working further on main page css

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -12,27 +12,29 @@ const AuthForm = props => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">
-        <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" />
-      </a>
+      <div id="auth-form-container">
+        <form onSubmit={handleSubmit} name={name}>
+          <div>
+            <label htmlFor="email">
+              <small>Email</small>
+            </label>
+            <input name="email" type="text" />
+          </div>
+          <div>
+            <label htmlFor="password">
+              <small>Password</small>
+            </label>
+            <input name="password" type="password" />
+          </div>
+          <div>
+            <button type="submit">{displayName}</button>
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
+        </form>
+        <a href="/auth/google">
+          <img src="https://developers.google.com/identity/images/btn_google_signin_light_normal_web.png" />
+        </a>
+      </div>
       <Home />
     </div>
   )
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,54 +7,60 @@ import {logout} from '../store'
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
     <nav>
-      <Link to="/" className="nav-title">
-        <i className="fas fa-carrot" />
-        Grace Shopper
-      </Link>
+      <div id="nav-text">
+        <Link to="/" className="nav-title">
+          <i className="fas fa-carrot" />
+          Grace Shopper
+        </Link>
 
-      {isLoggedIn ? (
-        <div className="logged-in-nav">
-          <ul id="nav-bar-ul">
-            <li>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/user" className="nav-item right-nav">
-                Account & Orders
-              </Link>
-            </li>
-            <li>
-              <a href="#" className="nav-item right-nav" onClick={handleClick}>
-                Logout
-              </a>
-            </li>
-            <li>
-              <Link to="/checkout" className="nav-item right-nav">
-                <i className="fas fa-shopping-cart" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <ul id="nav-bar-ul">
-            <li>
-              <Link to="/login" className="nav-item right-nav">
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link to="/signup" className="nav-item right-nav">
-                Sign Up
-              </Link>
-            </li>
-            <li>
-              <Link to="/checkout" className="nav-item right-nav">
-                <i className="fas fa-shopping-cart" />
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
+        {isLoggedIn ? (
+          <div className="logged-in-nav">
+            <ul id="nav-bar-ul">
+              <li>
+                {/* The navbar will show these links after you log in */}
+                <Link to="/user" className="nav-item right-nav">
+                  Account & Orders
+                </Link>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="nav-item right-nav"
+                  onClick={handleClick}
+                >
+                  Logout
+                </a>
+              </li>
+              <li>
+                <Link to="/checkout" className="nav-item right-nav">
+                  <i className="fas fa-shopping-cart" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <ul id="nav-bar-ul">
+              <li>
+                <Link to="/login" className="nav-item right-nav">
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link to="/signup" className="nav-item right-nav">
+                  Sign Up
+                </Link>
+              </li>
+              <li>
+                <Link to="/checkout" className="nav-item right-nav">
+                  <i className="fas fa-shopping-cart" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </nav>
   </div>
 )
